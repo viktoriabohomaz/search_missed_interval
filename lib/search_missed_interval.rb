705cd8daa20c9c123683fde0da1495d82fe79316
Viktoria <viktoriazlayaa@gmail.com>
2019-05-29 23:54:52 +0300

    add gem structure

---
+++ lib/search_missed_interval.rb
@@ -0,0 +1,6 @@
+require "search_missed_interval/version"
+
+module SearchMissedInterval
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/search_missed_interval/version.rb
@@ -0,0 +1,3 @@
+module SearchMissedInterval
+  VERSION = "0.1.0"
+end
